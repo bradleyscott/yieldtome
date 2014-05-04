63a47d7b3d63f267a96fc9e444a20bba358915d0
Bradley Scott <bradley.s.scott@gmail.com>
2014-05-04 16:49:33 +1200

    Checking in broken unit tests so I can get help.

---
+++ Web/test/unit/services/facebookServiceSpec.js
@@ -0,0 +1,51 @@
+// 'use strict';
+
+// /* jasmine specs for services go here */
+
+describe('The FacebookService', function() {
+
+    var $q, Facebook, FacebookService;
+
+    beforeEach(function() {
+        module('yieldtome.services', function($provide) {
+            Facebook = jasmine.createSpyObj('Facebook', ['getLoginStatus', 'login', 'api']); // Create Mocks 
+            $provide.value('Facebook', Facebook);
+        });
+
+        inject(function(_$q_, _FacebookService_) {
+            $q = _$q_;
+            FacebookService = _FacebookService_;
+        });
+    });
+
+    describe('has a getFacebookToken function', function() {
+
+        it("that should exist", function() {
+            expect(angular.isFunction(FacebookService.getFacebookToken)).toBe(true);
+        });
+
+        it("that should return an ApiToken if Facebook is connected and authorized", function() {
+            var getLoginStatusResponse = $q.defer();
+            getLoginStatusResponse.resolve( // Mock out the authResponse from Facebook
+                {
+                    status: 'connected',
+                    authResponse: {
+                        accessToken: 'ValidToken'
+                    }
+                });
+            Facebook.getLoginStatus.andReturn(getLoginStatusResponse.promise);
+
+            var loginStatusPromise = FacebookService.getFacebookToken();
+            loginStatusPromise.then(function(token) {
+                expect(token).not.toBeNull(); // The token should be ValidToken
+                expect(false).toBeTruthy(); // test for execution
+                expect(token).toBe('ValidToken');
+            });
+        });
+
+    });
+
+    // getUserInfo
+
+
+});
