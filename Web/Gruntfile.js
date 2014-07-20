eb294df41362e5f64a27cd8752ccddb14134ec92
Bradley Scott <bradley.s.scott@gmail.com>
2014-07-20 22:03:03 +1200

    Deployment changes to support ngFacebook

--- Web/Gruntfile.js
+++ Web/Gruntfile.js
@@ -70,7 +70,7 @@ module.exports = function(grunt) {
                         'bower_components/angular-route/angular-route.min.js.map',
                         'bower_components/angular-touch/angular-touch.min.js',
                         'bower_components/angular-touch/angular-touch.min.js.map',
-                        'bower_components/angular-facebook/lib/angular-facebook.js',
+                        'bower_components/ng-facebook/ngFacebook.js',
                         'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
                         'bower_components/angular-ui-sortable/sortable.min.js',
                         'bower_components/angular-loading-bar/build/loading-bar.min.js'
